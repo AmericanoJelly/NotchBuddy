c10f7aa6ab7d4a13c4806c7fdcb865d2b353cc6b
hailey <hailey@haileyui-MacBookPro.local>
2026-05-29 10:18:57 +0900

    feat: embed paw icon into app bundle via build.sh

--- build.sh
+++ build.sh
@@ -27,6 +27,40 @@ printf 'APPL????' > "$APP/Contents/PkgInfo"
 # default.gif 복사 (있을 경우)
 [ -f "$SRC/default.gif" ] && cp "$SRC/default.gif" "$APP/Contents/Resources/"
 
+# 앱 아이콘 생성 (.icns)
+ICONSET=$(mktemp -d)
+XCASSETS="$SRC/Assets.xcassets/AppIcon.appiconset"
+if [ -d "$XCASSETS" ]; then
+  cp "$XCASSETS/icon_16x16.png"      "$ICONSET/icon_16x16.png"
+  cp "$XCASSETS/icon_16x16@2x.png"   "$ICONSET/icon_16x16@2x.png"
+  cp "$XCASSETS/icon_32x32.png"      "$ICONSET/icon_32x32.png"
+  cp "$XCASSETS/icon_32x32@2x.png"   "$ICONSET/icon_32x32@2x.png"
+  cp "$XCASSETS/icon_128x128.png"    "$ICONSET/icon_128x128.png"
+  cp "$XCASSETS/icon_128x128@2x.png" "$ICONSET/icon_128x128@2x.png"
+  cp "$XCASSETS/icon_256x256.png"    "$ICONSET/icon_256x256.png"
+  cp "$XCASSETS/icon_256x256@2x.png" "$ICONSET/icon_256x256@2x.png"
+  cp "$XCASSETS/icon_512x512.png"    "$ICONSET/icon_512x512.png"
+  cp "$XCASSETS/icon_512x512@2x.png" "$ICONSET/icon_512x512@2x.png"
+  # iconutil은 확장자가 .iconset 이어야 함
+  ICONSET_DIR="$ICONSET/AppIcon.iconset"
+  mv "$ICONSET/icon_"*.png /tmp/ 2>/dev/null; true
+  mkdir -p "$ICONSET_DIR"
+  cp "$XCASSETS/icon_16x16.png"      "$ICONSET_DIR/icon_16x16.png"
+  cp "$XCASSETS/icon_16x16@2x.png"   "$ICONSET_DIR/icon_16x16@2x.png"
+  cp "$XCASSETS/icon_32x32.png"      "$ICONSET_DIR/icon_32x32.png"
+  cp "$XCASSETS/icon_32x32@2x.png"   "$ICONSET_DIR/icon_32x32@2x.png"
+  cp "$XCASSETS/icon_128x128.png"    "$ICONSET_DIR/icon_128x128.png"
+  cp "$XCASSETS/icon_128x128@2x.png" "$ICONSET_DIR/icon_128x128@2x.png"
+  cp "$XCASSETS/icon_256x256.png"    "$ICONSET_DIR/icon_256x256.png"
+  cp "$XCASSETS/icon_256x256@2x.png" "$ICONSET_DIR/icon_256x256@2x.png"
+  cp "$XCASSETS/icon_512x512.png"    "$ICONSET_DIR/icon_512x512.png"
+  cp "$XCASSETS/icon_512x512@2x.png" "$ICONSET_DIR/icon_512x512@2x.png"
+  iconutil -c icns "$ICONSET_DIR" -o "$APP/Contents/Resources/AppIcon.icns"
+  /usr/libexec/PlistBuddy -c "Set :CFBundleIconFile AppIcon" "$APP/Contents/Info.plist" 2>/dev/null \
+    || /usr/libexec/PlistBuddy -c "Add :CFBundleIconFile string AppIcon" "$APP/Contents/Info.plist"
+  rm -rf "$ICONSET"
+fi
+
 # 컴파일
 SDK=$(xcrun --show-sdk-path --sdk macosx)
 ARCH=$(uname -m)
